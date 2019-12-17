ca32ce5eb89ffbb2870b2e99fad508a914111dd5
Sudarshana Patil <sudarshanagopinath.patil@philips.com>
2019-12-17 17:04:29 +0530

    added recommendation queries

--- userRating.js
+++ userRating.js
@@ -4,9 +4,8 @@ const driver = neo4j.driver(
   'bolt://localhost:7687',
   neo4j.auth.basic('neo4j', 'sudri@123')
 )
-
 const session = driver.session()
-const userArr = [
+const userArr1 = [
   'Shweta',
   'Jagrutee',
   'Nilesh',
@@ -14,7 +13,7 @@ const userArr = [
   'Abhishek',
   'Sudarshana'
 ]
-
+const userArr = ['Jagrutee']
 addUser = userArr => {
   userArr.map(eachUser => {
     let query = `merge (${eachUser}:Person {name: '${eachUser}'}) RETURN ${eachUser}`
@@ -22,25 +21,25 @@ addUser = userArr => {
     const resultPromise = session.run(query)
     resultPromise.then(result => {
       //Get recipe Data
-      let getRecipeQ = 'MATCH (n:Recipe) RETURN n '
+      let getRecipeQ = 'MATCH (n:Recipe) RETURN n limit 15'
       const recipeData = session.run(getRecipeQ)
       recipeData.then(data => {
+        let count = 6
         let recipes = data.records.map(i => {
           //console.log(i['_fields'][0].properties.name, eachUser)
-          let recipeName = i['_fields'][0].properties.name;
-          let userName=eachUser
+          if (count >= 10) count = 1
+          let recipeName = i['_fields'][0].properties.name
+          let userName = eachUser
           let relationQ = `MATCH (a:Person),(b:Recipe)
           WHERE a.name = '${userName}' AND b.name = '${recipeName}'
-          CREATE (a)-[r:Rated { rating:5 }]->(b)
+          CREATE (a)-[r:Rated { rating:${++count} }]->(b)
           RETURN type(r), r.name`
           const recipeData = session.run(relationQ)
-          recipeData.then((res)=>{
+          recipeData.then(res => {
             console.log(res)
-
           })
-          recipeData.catch((err)=>{
+          recipeData.catch(err => {
             console.log(err)
-
           })
         })
       })
@@ -52,3 +51,16 @@ addUser = userArr => {
 }
 addUser(userArr)
 //Add user nodes
+
+findSimilarity = () => {
+  let relationS = `match  (p1:Person {name:'Nilesh'})-[r1:RATED]->(m:Recipe)<-[r2:RATED]-(p2:Person {name:'Sudarshana'})
+  RETURN m.name AS Recipe, r1.rating AS NileshRate, r2.rating AS SudduRate`
+  const recipeData = session.run(relationS)
+  recipeData.then(res => {
+    console.log(res)
+  })
+  recipeData.catch(err => {
+    console.log(err)
+  })
+}
+//findSimilarity()
