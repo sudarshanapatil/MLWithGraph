9b02d599f836e8a95b6b7b76257bc6b9211d97c7
Sudarshana Patil <patilsudarshana94@gmail.com>
2020-07-16 20:26:38 +0530

    async await added

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ try {
   )
   console.log('Connected to neo4j')
 } catch (error) {
-
   console.log("Error", error)
-
 }
 
 var mysql = require('mysql')
@@ -40,140 +38,108 @@ con.connect(function (err) {
 
 const cors = require('cors')
 const session = driver.session()
-const session2 = driver.session()
 const app = express()
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
   let { name, password } = req.body
   console.log('in login', req.body)
-  let query = `SELECT password FROM user where name='${name}'`
-  db.queryAsync(query)
-    .then(function (data) {
-      if (data[0].password === password) {
-        res.send({ code: 200, msg: `Login successful!` })
-      } else {
-        res.send({ code: 400, msg: `Unauthorized User` })
-      }
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  let query = `SELECT password FROM user where name='${name}'`;
+  try {
+    let data = await db.queryAsync(query);
+    if (data[0].password === password) {
+      res.send({ code: 200, msg: `Login successful!` })
+    } else {
+      res.send({ code: 400, msg: `Unauthorized User` })
+    }
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
-app.post('/register', (req, res) => {
+
+app.post('/register', async (req, res) => {
   let userName = req.body.name
   let password = req.body.password
-  let query = `insert into user (name,password) values ('${userName}',"${password}")`
-  db.queryAsync(query)
-    .then(function (rows) {
-      console.log(rows)
-      res.send({ code: 200, msg: 'successfully inserted!' })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  let query = `insert into user (name,password) values ('${userName}',"${password}")`;
+  try {
+    await db.queryAsync(query);
+    res.send({ code: 200, msg: 'successfully inserted!' })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
+
+
 })
-app.post('/raterecipes', (req, res) => {
+app.post('/raterecipes', async (req, res) => {
   //TODO:jwttoken implemetion need to be added
-console.log(req.body,"====")
-  let recipeId = req.body.recipeId
-  let rating = req.body.rating
-  let userName = req.body.user
-  console.log(recipeId, 'recipeId', rating, userName)
-  console.log('rateRecipes', recipeId)
+  console.log(req.body, "====")
+  let { recipeId, rating, user } = req.body
   let query2 = `MATCH (a:Person),(b:Recipe)
-  WHERE a.name = '${userName}' AND b.id = '${recipeId}'
+  WHERE a.name = '${user}' AND b.id = '${recipeId}'
   Merge (a)-[r:Rated ]->(b)
   set  r.rating=${rating}`
-  const resultPromise = session.run(query2)
-
-  resultPromise.then(result => {
-    let assignNewSimilarity = `MATCH (p1:Person)-[x:Rated]->(m:Recipe)<-[y:Rated]-(p2:Person)
-    WITH  SUM(x.rating * y.rating) AS xyDotProduct,
-          SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength,
-          SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.rating) | yDot + b^2)) AS yLength,
-          p1, p2
-    MERGE (p1)-[s:SIMILARITY]-(p2)
-    SET   s.similarity = xyDotProduct / (xLength * yLength)`
-    const res2 = session.run(assignNewSimilarity)
-    res2.then(() => {
-      res.send({ code: 200, message: 'Successfully saved' })
-    })
-
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  let assignNewSimilarity = `MATCH (p1:Person)-[x:Rated]->(m:Recipe)<-[y:Rated]-(p2:Person)
+  WITH  SUM(x.rating * y.rating) AS xyDotProduct,
+        SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength,
+        SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.rating) | yDot + b^2)) AS yLength,
+        p1, p2
+  MERGE (p1)-[s:SIMILARITY]-(p2)
+  SET   s.similarity = xyDotProduct / (xLength * yLength)`;
+  try {
+    await session.run(query2)
+    await session.run(assignNewSimilarity)
+    res.send({ code: 200, message: 'Successfully saved' })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.get('/getallingredients', (req, res) => {
+app.get('/getallingredients', async (req, res) => {
   console.log('in getallingredients')
-  let query = `MATCH (n:Ingredient) RETURN n`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
-    console.log(result, 'data')
+  let query = `MATCH (n:Ingredient) RETURN n`;
+  try {
+    let result = await session.run(query)
     let ingredients = result.records.map(i => {
       return i['_fields'][0].properties.name
     })
     ingredients = ingredients.sort()
     res.send(ingredients)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.post('/getauthorRecipes', (req, res) => {
-  console.log('in getallingredients')
-  let authorName = req.body.authorName;
-  let query = `MATCH (p1:Author {name:${authorName}})-[s:WROTE]-(p2:Recipe) return p2.name,p2.description`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
-    console.log(result, 'data')
-    let ingredients = result.records.map(i => {
-      return i['_fields'][0].properties.name
-    })
-    ingredients = ingredients.sort()
-    res.send(ingredients)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
-})
 
-app.get('/getallrecipes', (req, res) => {
+
+app.get('/getallrecipes', async (req, res) => {
   console.log('in getallrecipes')
   let modifiedQ = `MATCH (r:Recipe)
   WHERE (r)-[:CONTAINS_INGREDIENT]->()
   RETURN r.name AS recipe,r.id as id, 
          [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] 
          AS ingredients`
-  const resultPromise = session.run(modifiedQ)
-  resultPromise.then(result => {
-    console.log(result.records.length)
+  try {
+    let result = await session.run(modifiedQ)
     let ingredients = result.records.map(i => {
       return {
         recipeName: i['_fields'][0],
         id: i['_fields'][1],
         ingredients: i['_fields'][2]
-
       }
     })
     ingredients = ingredients.sort()
     res.send(ingredients)
-  })
-  resultPromise.catch(err => {
-    console.log(err, 'here err')
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.post('/getrecipes', (req, res) => {
+app.post('/getrecipes', async (req, res) => {
   console.log('in getrecipes', req.body)
-  let ingredients = req.body.ingredients
-
+  let { ingredients } = req.body
   let finalQ = ingredients.map((i, index) => {
     if (index === 0)
       return `WHERE (r)-[:CONTAINS_INGREDIENT]->(:Ingredient {name: "${ingredients[0]}"})`
@@ -181,46 +147,40 @@ app.post('/getrecipes', (req, res) => {
   })
 
   finalQ = finalQ.join(' ')
+  let query = `MATCH (r:Recipe) ${finalQ}
+  RETURN r.name AS recipe, 
+  [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] 
+  AS ingredients`
   try {
-    let query = `MATCH (r:Recipe) ${finalQ}
-         RETURN r.name AS recipe, 
-         [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] 
-         AS ingredients`
     console.log(query, ' : Query on database')
-    const resultPromise = session.run(query)
-    resultPromise.then(result => {
-      let recipes = result.records.map(i => {
-        return {
-          recipe: i['_fields'][0],
-          ingredients: i['_fields'][1],
-          score: i['_fields'][1].length
-        }
-      })
+    let result = await session.run(query)
+    let recipes = result.records.map(i => {
+      return {
+        recipe: i['_fields'][0],
+        ingredients: i['_fields'][1],
+        score: i['_fields'][1].length
+      }
+    })
+    recipes = recipes.sort(function (a, b) {
+      let keyA = a.score,
+        keyB = b.score
+      if (keyA < keyB) return -1
+      if (keyA > keyB) return 1
+      return 0
+    })
+    res.send(recipes)
 
-      recipes = recipes.sort(function (a, b) {
-        let keyA = a.score,
-          keyB = b.score
-        if (keyA < keyB) return -1
-        if (keyA > keyB) return 1
-        return 0
-      })
-      res.send(recipes)
-    })
-    resultPromise.catch(err => {
-      console.log(err)
-      res.send(err)
-    })
   } catch (err) {
     console.log('Error : ', err)
-    res.send(err)
+    res.send({ code: 500, error })
   }
 })
 
-app.post('/getrecipelevel', (req, res) => {
+app.post('/getrecipelevel', async (req, res) => {
   let level = req.body.skillLevel
   let query = `MATCH (n:Recipe{ skillLevel: '${level}' }) RETURN n limit 20`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
+  try {
+    let result = await session.run(query)
     let finalData = result.records.map(recipe => {
       let data = recipe['_fields'][0].properties;
       return {
@@ -231,32 +191,28 @@ app.post('/getrecipelevel', (req, res) => {
       }
     })
     res.send(finalData)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.post('/getwrittenrecipe', (req, res) => {
-  console.log("hete")
-  let authorName = req.body.authorName
+app.post('/getwrittenrecipe', async (req, res) => {
+  let { authorName } = req.body;
   console.log(authorName, "authorName")
-  let query = `MATCH (n { name: '${authorName}' })-[r:WROTE]->(m) return m`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
-    console.log(result, "res");
+  let query = `MATCH (n { name: '${authorName}' })-[r:WROTE]->(m) return m`;
+  try {
+    const result = await session.run(query)
     let finalData = result.records.map(recipe => {
       console.log(recipe['_fields'][0].properties, "data")
       return recipe['_fields'][0].properties
     })
     res.send(finalData)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.post('/addrecipe', (req, res) => {
+app.post('/addrecipe', async (req, res) => {
   let {
     recipeName,
     selected,
@@ -289,65 +245,26 @@ app.post('/addrecipe', (req, res) => {
    MERGE (a)-[:WROTE]->(${recipeName});`
   let query4 = `MATCH (n { name: '${authorName}' }) - [r: WROTE] -> (m) return m`
 
-  const resultPromise1 = session.run(query1)
-  resultPromise1.then(() =>
-    session.run(query2)
-  )
-    .then(() =>
-      session.run(query3)
-    )
-    .then(() =>
-      session.run(query4)
-    )
-    .then(result => {
-      let finalData = result.records.map(recipe => {
-        console.log(recipe['_fields'][0].properties, "data")
-        return recipe['_fields'][0].properties
-      })
-      console.log("successfully added recipe")
-      res.send({ code: 200, authorRecipes: finalData })
-      // res.send(finalData)
+  try {
+    await session.run(query1)
+    await session.run(query2)
+    await session.run(query3)
+    let result = await session.run(query4)
+    let finalData = result.records.map(recipe => {
+      console.log(recipe['_fields'][0].properties, "data")
+      return recipe['_fields'][0].properties
     })
+    console.log("successfully added recipe")
+    res.send({ code: 200, authorRecipes: finalData })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.get('/getall', (req, res) => {
-  const nodeName = `Recipe`
-  let query = `MATCH (r:Recipe) WHERE (r)-[:CONTAINS_INGREDIENT]->(:Ingredient {name: "chilli"}) 
-  RETURN r.name AS recipe, 
-         [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] 
-         AS ingredients`
-
-  resultPromise.then(result => {
-    res.send(result)
-    // on application exit:
-    driver.close()
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
-})
-
-app.get('/getdetailedrecipe', (req, res) => {
-  console.log('in getdetailedrecipe')
-  const nodeName = `Recipe`
-  let query = `MATCH (r:Recipe) WHERE (r)-[:CONTAINS_INGREDIENT]->(:Ingredient {name: "chilli"}) 
-  RETURN r.name AS recipe, 
-         [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] 
-         AS ingredients`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
-    console.log(result, 'data')
-    res.send(result)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
-})
-
-app.post('/getuserrecommendation', (req, res) => {
-  console.log('in getuserrecommendation ', req.body.userName)
-  let userName = req.body.userName;
-  let query = `MATCH    (b:Person)-[r:Rated]->(m:Recipe), (b)-[s:SIMILARITY]-(a:Person {name:'${userName}'})
+app.post('/getuserrecommendation', async (req, res) => {
+  let { userName } = req.body;
+  let query = `MATCH (b:Person)-[r:Rated]->(m:Recipe), 
+  (b)-[s:SIMILARITY]-(a:Person {name:'${userName}'})
   WHERE    NOT((a)-[:Rated]->(m))
   WITH     m, s.similarity AS similarity, r.rating AS rating
   ORDER BY m.name, similarity DESC
@@ -355,75 +272,35 @@ app.post('/getuserrecommendation', (req, res) => {
   WITH     recipe, REDUCE(s = 0, i IN ratings | s + i)*1.0 / LENGTH(ratings) AS reco
   ORDER BY reco DESC
   RETURN   recipe AS Recipe, reco AS Recommendation`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
-    console.log(result, 'data of recommendation')
+  try {
+    const result = await session.run(query)
     let recipes = result.records.map(i => {
       return i['_fields'][0]
     })
     res.send(recipes)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
 
-app.post('/getsimilaruser', (req, res) => {
-  let userName = req.body.userName
+app.post('/getsimilaruser', async (req, res) => {
+  let { userName } = req.body
   console.log('in getsimilaruser')
   let query = `MATCH    (p1:Person {name:'${userName}'})-[s:SIMILARITY]-(p2:Person)
   WITH     p2, s.similarity AS sim
   ORDER BY sim DESC
   LIMIT    5
-  RETURN   p2.name AS Neighbor, sim AS Similarity`
-  const resultPromise = session.run(query)
-  resultPromise.then(result => {
+  RETURN   p2.name AS Neighbor, sim AS Similarity`;
+  try {
+    let result = await session.run(query)
     let userList = result.records.map(i => {
       return { name: i['_fields'][0], similarity: i['_fields'][1].toFixed(2) }
     })
     res.send(userList)
-  })
-  resultPromise.catch(err => {
-    console.log(err)
-  })
+  } catch (error) {
+    res.send({ code: 500, error })
+  }
 })
-
-app.post('/getuserrating', (req, res) => {
-  console.log('in get user rating')
-  let getRecipeQ = 'MATCH (n:Recipe) RETURN n limit 15'
-  const recipeData = session.run(getRecipeQ)
-  recipeData.then(data => {
-    let obj = {}
-    data.records.map(i => {
-      let recipeName = i['_fields'][0].properties.name
-      let getRecipeRatingQ = `MATCH (:Recipe {name: '${recipeName}'})-[r:Rated]-(b) RETURN r,b`
-      const userRating = session.run(getRecipeRatingQ)
-      userRating.then(userRatingData => {
-        userRatingData.records.map(userData => {
-          let userName = userData['_fields'][1].properties.name
-          let userRating = userData['_fields'][0].properties.rating.low
-          if (!obj[userName]) {
-            obj[userName] = [
-              {
-                recipeName,
-                userRating
-              }
-            ]
-          } else {
-            obj[userName].push({
-              recipeName,
-              userRating
-            })
-          }
-        })
-      })
-      userRating.catch(err => {
-        console.log(err)
-      })
-    })
-  })
-})
-
 // Starting server
 const port = 1337
 app.listen(port)
--- usefullScripts/userRating.js
+++ usefullScripts/userRating.js
@@ -1,199 +1,105 @@
 const neo4j = require('neo4j-driver')
 // Neo4j credentials
 const driver = neo4j.driver(
-  'bolt://localhost:11002',
+  'bolt://localhost:11008',
   neo4j.auth.basic('neo4j', 'sudri@123')
 )
 const session = driver.session()
-const userArr1 = [
-  'Shweta',
-  'Jagrutee',
-  'Nilesh',
-  'Akshay',
-  'Abhishek',
-  'Sudarshana'
-]
 const userArr = [
-  'Madhav',
-  'Priya',
-  'Sukrut',
-  'Arnav',
-  'Gaurav',
-  'Nirav',
-  'Radhya',
-  'Stuti',
-  'Akshaya',
-  'Manas',
-  'Mrinal',
-  'Madhura',
-  'Keshav',
-  'Nritya',
-  'Prisha',
-  'Pratyusha',
-  'Mohan',
-  'Mihika',
-  'Nikhil',
-  'Girisha',
-  'Mithila',
-  'Janak',
-  'Jidnyasa',
-  'Aadnya',
-  'Sneha',
-  'Smruti',
-  'Samiksha',
-  'Spruha',
-  'Titiksha',
-  'Umesh',
-  'Vidit',
-  'Prasanna',
-  'Ramesh',
-  'Sarvesh',
-  'Parth',
+  //'Madhav',
+  // 'Priya',
+  // 'Sukrut',
+  // 'Arnav',
+  // 'Gaurav',
+  // 'Nirav',
+  //'Radhya',
+  //'Stuti',
+  // 'Akshaya',
+  // 'Manas',
+  //'Mrinal',
+  //'Madhura',
+  // 'Keshav',
+  // 'Nritya',
+  // 'Prisha',
+  // 'Pratyusha',
+  // 'Mohan',
+  // 'Mihika',
+  // 'Nikhil',
+  // 'Girisha',
+  // 'Mithila',
+  // 'Janak',
+  // 'Jidnyasa',
+  // 'Aadnya',
+  // 'Sneha',
+  // 'Smruti',
+  // 'Samiksha',
+  // 'Spruha',
+  // 'Titiksha',
+  // 'Umesh',
+  // 'Vidit',
+  // 'Prasanna',
+  // 'Ramesh',
+  // 'Sarvesh',
+  // 'Parth',
   'Vineet',
-  'Anshul',
-  'Praful',
-  'Aakash',
-  'Ninad',
-  'Ninant'
+  // 'Anshul',
+  // 'Praful',
+  // 'Aakash',
+  // 'Ninad',
+  // 'Ninant'
 
 ]
-addUser = userArr => {
-  userArr.map((eachUser,key) => {
-    console.log(key,"key")
-    let query = `merge (${eachUser}:Person {name: '${eachUser}'}) RETURN ${eachUser}`
-    console.log(query)
-    const resultPromise = session.run(query)
-    resultPromise.then(result => {
-      //Get recipe Data
-      // let count;
-      // if(key>20)
-      //  count=parseInt(key)-20
-      //  else
-      //  count=15
-      let getRecipeQ = `MATCH (n:Recipe) RETURN n limit 8` 
-      const recipeData = session.run(getRecipeQ)
-      recipeData.then(data => {
-        // console.log(data,"recipes")
-        let count = key
-        let recipes = data.records.map(i => {
-          //console.log(i,"i")
-          console.log(i['_fields'][0].properties.name, eachUser)
-          if (count >= 10) count = 3
-          let recipeName = i['_fields'][0].properties.name
-          let userName = eachUser
-          let relationQ = `MATCH (a:Person),(b:Recipe)
-          WHERE a.name = '${userName}' AND b.name = '${recipeName}'
-          CREATE (a)-[r:Rated { rating:${++count} }]->(b)
-          RETURN type(r), r.name`
-          const recipeData = session.run(relationQ)
-          recipeData.then(res => {
-           // console.log(res)
-          })
-          recipeData.catch(err => {
-            console.log(err)
-          })
-        })
-     })
+addUser = async userArr => {
+  for (let user of userArr) {
+    await createUser(user)
+    let recipes = await findRecipe()
+    console.log(recipes)
+    for (let i in recipes)
+      await rateRecipe(recipes[i], user, i)
+  }
+}
+
+addUser(userArr)
+
+async function createUser(eachUser) {
+  let query = `merge (${eachUser}:Person {name: '${eachUser}'}) RETURN ${eachUser}`
+  console.log(query)
+  try {
+    await session.run(query)
+  } catch (error) {
+  }
+}
+
+async function findRecipe() {
+  console.log("in find recipe")
+  // let getRecipeQ = `MATCH (n:Recipe) RETURN n limit 5`;
+  let getRecipeQ = `MATCH (r:Recipe)
+     WHERE (r)-[:CONTAINS_INGREDIENT]->(:Ingredient {name: "almond"})
+    RETURN r.name AS recipe, 
+    [(r)-[:CONTAINS_INGREDIENT]->(i) | i.name] limit 9`
+  try {
+    let result = await session.run(getRecipeQ)
+    console.log(result)
+    let data = result.records.map((i) => {
+      console.log(i['_fields'][0])
+      return i['_fields'][0]
     })
-    resultPromise.catch(err => {
-      console.log(err)
-    })
-  })
-}
-//addUser(userArr)
-//Add user nodes
+    return data;
+  } catch (error) {
 
-findSimilarity = () => {
-  let relationS = `match  (p1:Person {name:'Nilesh'})-[r1:RATED]->(m:Recipe)<-[r2:RATED]-(p2:Person {name:'Sudarshana'})
-  RETURN m.name AS Recipe, r1.rating AS NileshRate, r2.rating AS SudduRate`
-  const recipeData = session.run(relationS)
-  recipeData.then(res => {
-    console.log(res)
-  })
-  recipeData.catch(err => {
-    console.log(err)
-  })
+  }
 }
 
-assignSimilarity = () => {
-  let query = `MATCH (p1:Person)-[x:Rated]->(m:Recipe)<-[y:Rated]-(p2:Person)
-  WITH  SUM(x.rating * y.rating) AS xyDotProduct,
-        SQRT(REDUCE(xDot = 0.0, a IN COLLECT(x.rating) | xDot + a^2)) AS xLength,
-        SQRT(REDUCE(yDot = 0.0, b IN COLLECT(y.rating) | yDot + b^2)) AS yLength,
-        p1, p2
-  MERGE (p1)-[s:SIMILARITY]-(p2)
-  SET   s.similarity = xyDotProduct / (xLength * yLength)`
+function rateRecipe(recipeName, userName, count) {
+  console.log(recipeName, userName)
+  let relationQ = `MATCH (a:Person),(b:Recipe)
+  WHERE a.name = '${userName}' AND b.name = '${recipeName}'
+  CREATE (a)-[r:Rated { rating:${++count} }]->(b)
+  RETURN type(r), r.name`
+  try {
+    let res = session.run(relationQ)
+    return res;
+  } catch (error) {
+
+  }
 }
-
-insertInDb=()=>{
- let query=`CREATE INDEX ON :Recipe(id);
- CREATE INDEX ON :Ingredient(name);
- CREATE INDEX ON :Keyword(name);
- CREATE INDEX ON :DietType(name);
- CREATE INDEX ON :Author(name);
- CREATE INDEX ON :Collection(name);
- :params jsonFile => "https://raw.githubusercontent.com/mneedham/bbcgoodfood/master/stream_all.json";
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.title AS title,
-        value.page.article.description AS description,
-        value.page.recipe.cooking_time AS cookingTime,
-        value.page.recipe.prep_time AS preparationTime,
-        value.page.recipe.skill_level AS skillLevel
- MERGE (r:Recipe {id: id})
- SET r.cookingTime = cookingTime,
-     r.preparationTime = preparationTime,
-     r.name = title,
-     r.description = description,
-     r.skillLevel = skillLevel;
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.article.author AS author
- MERGE (a:Author {name: author})
- WITH a,id
- MATCH (r:Recipe {id:id})
- MERGE (a)-[:WROTE]->(r);
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.recipe.ingredients AS ingredients
- MATCH (r:Recipe {id:id})
- FOREACH (ingredient IN ingredients |
-   MERGE (i:Ingredient {name: ingredient})
-   MERGE (r)-[:CONTAINS_INGREDIENT]->(i)
- );
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.recipe.keywords AS keywords
- MATCH (r:Recipe {id:id})
- FOREACH (keyword IN keywords |
-   MERGE (k:Keyword {name: keyword})
-   MERGE (r)-[:KEYWORD]->(k)
- );
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.recipe.diet_types AS dietTypes
- MATCH (r:Recipe {id:id})
- FOREACH (dietType IN dietTypes |
-   MERGE (d:DietType {name: dietType})
-   MERGE (r)-[:DIET_TYPE]->(d)
- );
- CALL apoc.load.json($jsonFile) YIELD value
- WITH value.page.article.id AS id,
-        value.page.recipe.collections AS collections
- MATCH (r:Recipe {id:id})
- FOREACH (collection IN collections |
-   MERGE (c:Collection {name: collection})
-   MERGE (r)-[:COLLECTION]->(c)
- );`
-
- const recipeData = session.run(query)
-  recipeData.then(res => {
-    console.log(res)
-  })
-  recipeData.catch(err => {
-    console.log(err)
-  })
-}
-
-insertInDb()
-//findSimilarity()
